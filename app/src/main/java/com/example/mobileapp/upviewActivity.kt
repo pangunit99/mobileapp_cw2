895c8f3811cd27952768fd14c69614de6cf96e6d
pangunit99 <railpang1999@gmail.com>
2023-06-21 17:01:34 +0800

    finish the recyclerview onClickListener to get the key

--- app/src/main/java/com/example/mobileapp/upviewActivity.kt
+++ app/src/main/java/com/example/mobileapp/upviewActivity.kt
@@ -2,18 +2,24 @@ package com.example.mobileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobileapp.databinding.ActivityMainBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import java.lang.reflect.Array
+import kotlin.math.log
 
 class upviewActivity : AppCompatActivity() {
     private lateinit var db: DatabaseReference
     private lateinit var binding:ActivityMainBinding
     private lateinit var productArratlist:ArrayList<product_data>
     private lateinit var view1:RecyclerView
-
+    val list = arrayListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,21 +28,55 @@ class upviewActivity : AppCompatActivity() {
         view1 = findViewById<RecyclerView>(R.id.view1)
         view1.layoutManager = LinearLayoutManager(this)
         view1.setHasFixedSize(true)
+        view1.itemAnimator = DefaultItemAnimator()
         productArratlist = arrayListOf()
         fetchdata()
+        val helper = ItemTouchHelper(object  : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                if(direction == ItemTouchHelper.RIGHT) {
+                    val item = viewHolder.adapterPosition
+                    productArratlist.removeAt(item)
+                    list.removeAt(item)
+                    view1.adapter?.notifyDataSetChanged()
+                }
+            }
+
+        })
+        helper.attachToRecyclerView(view1)
+
+
     }
 
     fun fetchdata(){
         db = FirebaseDatabase.getInstance().getReference("product")
+        Log.d("GET DATA", db.child("save").key.toString())
         db.child("save").get().addOnSuccessListener { documents->
             for (doc in documents.children){
+                val ky = doc.key.toString()
+                list.add(ky)
+                Log.d("GET DATA", doc.key.toString())
 
                 val product = doc.getValue(product_data::class.java)
                 productArratlist.add(product!!)
 
             }
+            var adapter = viewAdapter(this,productArratlist)
+            view1.adapter= adapter
+            adapter.setOnItemClickListener(object :viewAdapter.onItemClickListener{
+                override fun onItemClick(position: Int) {
+                    Toast.makeText(this@upviewActivity,"Youclick on item key : ${list[position]}",Toast.LENGTH_SHORT).show()
 
-            view1.adapter= viewAdapter(this,productArratlist)
+
+                }
+            })
 
 
         }
--- app/src/main/java/com/example/mobileapp/viewAdapter.kt
+++ app/src/main/java/com/example/mobileapp/viewAdapter.kt
@@ -1,21 +1,33 @@
 package com.example.mobileapp
 
 import android.content.Context
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
 class viewAdapter(val context: Context, private val pd:List<product_data>) : RecyclerView.Adapter<product_view_holder>() {
+    private lateinit var  mListener:onItemClickListener
+    interface  onItemClickListener{
+        fun onItemClick(position: Int)
+    }
+
+    fun setOnItemClickListener(listener:onItemClickListener){
+        mListener = listener
+    }
+
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): product_view_holder {
-        return product_view_holder(
-            LayoutInflater.from(parent.context).inflate(R.layout.viewpd,parent,false)
 
+        val itemView =LayoutInflater.from(parent.context).inflate(R.layout.viewpd,parent,false)
+        return product_view_holder(
+            itemView,mListener
         )
 
     }
@@ -34,11 +46,18 @@ class viewAdapter(val context: Context, private val pd:List<product_data>) : Rec
 
 }
 
-class product_view_holder(itemView: View):RecyclerView.ViewHolder(itemView){
+class product_view_holder(itemView: View, listener:viewAdapter.onItemClickListener):RecyclerView.ViewHolder(itemView){
 
     val itemName = itemView.findViewById<TextView>(R.id.productname)
     val price = itemView.findViewById<TextView>(R.id.pdprice)
     val image = itemView.findViewById<ImageView>(R.id.imageView3)
+
+    init{
+        itemView.setOnClickListener{
+            listener.onItemClick(adapterPosition)
+        }
+
+    }
 
 
 }
